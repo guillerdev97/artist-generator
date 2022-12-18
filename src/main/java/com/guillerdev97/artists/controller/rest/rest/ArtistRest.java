10ac6559a2b4ec9adf135a653beb4bceb74ba226
Guillermo García del Río <guillermieres97@gmail.com>
2022-12-18 13:13:49 +0100

    create getAllArtists test of the controller

--- src/main/java/com/guillerdev97/artists/controller/rest/rest/ArtistRest.java
+++ src/main/java/com/guillerdev97/artists/controller/rest/rest/ArtistRest.java
@@ -3,12 +3,14 @@ package com.guillerdev97.artists.controller.rest.rest;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ArtistRest implements Serializable {
